8aca47e3903d54fe0da364ec4ee806c15c33d01a
Landon Reed <landontreed@gmail.com>
2017-09-25 18:44:48 -0400

    load all algarve tables (WIP: calendars, calendar_dates, and stop_times are broken)

--- src/main/java/com/conveyal/datatools/manager/utils/AlgarveConstants.java
+++ src/main/java/com/conveyal/datatools/manager/utils/AlgarveConstants.java
@@ -29,52 +29,26 @@ public final class AlgarveConstants {
         // restrict instantiation
     }
 
+    public static final String CALENDARS_TABLE = "dbo.PeriodosAnuais";
+    public static final String CALENDAR_ID = "IdPeriodo"; // FIXME: not actually service_id
+    public static final String START_DATE = "DataInicio";
+    public static final String END_DATE = "DataFim";
+
+    public static final String ROUTES_TABLE = "dbo.Carreiras";
+    public static final String ROUTE_SHORT_NAME = "CodCarOperador";
+    public static final String ROUTE_ID = "IdCarreira";
+
     public static final String NAME = "Designacao";
+    public static final String STOPS_TABLE = "dbo.Paragem";
     public static final String STOP_ID = "IdParagem";
+
     public static final String LAT_LNG = "lat_lng";
     public static final String SHAPE = "shape.STAsText() as " + LAT_LNG;
 
-    // FIXME: use Table objects to build schema for Algarve
-//    public static final STOPS = new Table();
+    public static final String TRIPS_TABLE = "CarreiraCirculacaoFrequencia";
+    public static final String SERVICE_ID = "IdPerFreq";
+    public static final String TRIP_ID = "IdCarCircPerFreqSen";
 
-    public static final EntityPopulator<Stop> STOP = (result, columnForName) -> {
-        // FIXME: CRS may need to be defined differently. Transform has "lenient" param set to true because
-        // Bursa-Wolf parameters are "missing"? http://docs.geotools.org/stable/userguide/faq.html#q-bursa-wolf-parameters-required
-        CoordinateReferenceSystem sourceCRS = null;
-        try {
-            sourceCRS = CRS.decode("EPSG:102165");
-        } catch (FactoryException e) {
-            e.printStackTrace();
-        }
-        Stop stop = new Stop();
-
-        String geom = result.getString(LAT_LNG);
-        // geom string looks something like POINT (-41849.9930 89209.99109)
-        String[] geomArray = geom.split("\\(|\\)|\\s");
-        Coordinate coordinate = new Coordinate(Double.parseDouble(geomArray[3]), Double.parseDouble(geomArray[2]));
-        MathTransform transform = null;
-        Coordinate targetCoordinate = null;
-        try {
-            transform = CRS.findMathTransform(sourceCRS, DefaultGeographicCRS.WGS84, true);
-            targetCoordinate = JTS.transform( coordinate, null, transform );
-        } catch (FactoryException e) {
-            e.printStackTrace();
-        } catch (TransformException e) {
-            e.printStackTrace();
-        }
-
-        stop.stop_id        = getStringIfPresent(result, "IdParagem", columnForName);
-//        stop.stop_code      = getStringIfPresent(result, "stop_code", columnForName);
-        stop.stop_name      = getStringIfPresent(result, "Designacao", columnForName);
-        stop.stop_desc      = getStringIfPresent(result, "stop_desc", columnForName);
-        stop.stop_lon = targetCoordinate.x;
-        stop.stop_lat = targetCoordinate.y;
-//        stop.zone_id        = getStringIfPresent(result, "zone_id", columnForName);
-//        stop.parent_station = getStringIfPresent(result, "parent_station", columnForName);
-//        stop.stop_timezone  = getStringIfPresent(result, "stop_timezone", columnForName);
-//        stop.stop_url = null; //new URL(getStringIfPresent(result, "stop_url",  columnForName));
-//        stop.location_type = getIntIfPresent(result, "location_type", columnForName);
-//        stop.wheelchair_boarding = Integer.toString(getIntIfPresent(result, "wheelchair_boarding", columnForName));
-        return stop;
-    };
+    public static final String ARRIVAL_TIME = "OrdemPrevious";
+    public static final String STOP_SEQUENCE = "IdCarSen";
 }
