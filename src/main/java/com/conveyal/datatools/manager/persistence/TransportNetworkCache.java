9e063efb6dfacf34aa9b956add5783d954858c02
Landon Reed <landontreed@gmail.com>
2017-10-18 16:06:52 -0400

    add TransportNetworkCache constructor to control cacheSize, duration, and timeUnits

--- src/main/java/com/conveyal/datatools/manager/persistence/TransportNetworkCache.java
+++ src/main/java/com/conveyal/datatools/manager/persistence/TransportNetworkCache.java
@@ -10,8 +10,8 @@ import com.google.common.cache.RemovalListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -28,32 +28,38 @@ public class TransportNetworkCache {
 //    private final Weigher<String, TransportNetwork> weigher = (k, transportNetwork) ->
 //            transportNetwork.gridPointSet.featureCount();
     private final LoadingCache<String, TransportNetwork> transportNetworkCache;
-    private final Set<String> loadedTransportNetworks = new HashSet<>();
+    private final Map<String, Long> loadedTransportNetworks = new HashMap<>();
+    private static final int DEFAULT_CACHE_SIZE = 3;
+    private static final long DEFAULT_DURATION = 2;
+    private static final TimeUnit DEFAULT_TIME_UNIT = TimeUnit.MINUTES;
+    public final int cacheSize;
+    public final long duration;
+    public final TimeUnit timeUnit;
 
     /**
      * Listens for removal from cache (due to expiration or size restriction) and sets transportNetwork to null for GC.
      */
     private final RemovalListener<String, GTFSFeed> removalListener = removalNotification -> {
-        // Set version transportNetwork to null on removal to initiate garbage collection.
-        // FIXME: is there a better way to initiate gc?
         String feedVersionId = removalNotification.getKey();
         LOG.info("Evicting transport network. Cause: {}; ID: {}", removalNotification.getCause(), feedVersionId);
-        FeedVersion version = FeedVersion.get(feedVersionId);
         loadedTransportNetworks.remove(feedVersionId);
     };
 
-    public TransportNetworkCache () {
-         transportNetworkCache = CacheBuilder.newBuilder()
-                 // we use SoftReferenced values because we have the constraint that we don't want more than one
-                 // copy of a particular TransportNetwork object around; that would mean multiple MapDBs are pointing
-                 // at the same file, which is bad.
-                 // TODO: should we be using soft values?
+    public TransportNetworkCache (int cacheSize, long duration, TimeUnit timeUnit) {
+        this.cacheSize = cacheSize;
+        this.duration = duration;
+        this.timeUnit = timeUnit;
+        transportNetworkCache = CacheBuilder.newBuilder()
+                // we use SoftReferenced values because we have the constraint that we don't want more than one
+                // copy of a particular TransportNetwork object around; that would mean multiple MapDBs are pointing
+                // at the same file, which is bad.
+                // TODO: should we be using soft values?
                 .softValues()
                 .expireAfterAccess(10, TimeUnit.MINUTES)
-                 // TODO: Use maximumWeight instead of using maximumSize for better resource consumption estimate?
-//            .maximumWeight(100000)
-//            .weigher(weigher)
-                .maximumSize(3)
+                // TODO: Use maximumWeight instead of using maximumSize for better resource consumption estimate?
+//                .maximumWeight(100000)
+//                .weigher(weigher)
+                .maximumSize(cacheSize)
                 .removalListener(removalListener)
                 .build(new CacheLoader() {
                     @Override
@@ -73,12 +79,16 @@ public class TransportNetworkCache {
                 });
     }
 
+    public TransportNetworkCache () {
+         this(DEFAULT_CACHE_SIZE, DEFAULT_DURATION, DEFAULT_TIME_UNIT);
+    }
+
     /**
      * Wraps get method on cache to handle any exceptions.
      */
     public TransportNetwork getTransportNetwork (String feedVersionId) throws ExecutionException {
         try {
-            loadedTransportNetworks.add(feedVersionId);
+            loadedTransportNetworks.put(feedVersionId, System.currentTimeMillis());
             TransportNetwork tn = transportNetworkCache.get(feedVersionId);
             return tn;
         } catch (Exception e) {
@@ -89,6 +99,20 @@ public class TransportNetworkCache {
     }
 
     public boolean containsTransportNetwork (String feedVersionId) {
-        return loadedTransportNetworks.contains(feedVersionId);
+        return loadedTransportNetworks.containsKey(feedVersionId);
+    }
+
+    public boolean isAtCapacity () {
+        return cacheSize == loadedTransportNetworks.size();
+    }
+
+    /**
+     * Calculates the time since the earliest active transport network was loaded into the cache, or returns
+     * Long.MAX_VALUE if the cache is empty.
+     */
+    public long getTimeSinceEarliestLoad() {
+        return loadedTransportNetworks.size() > 0
+                ? loadedTransportNetworks.values().stream().min(Long::compare).get() - System.currentTimeMillis()
+                : Long.MAX_VALUE;
     }
 }
